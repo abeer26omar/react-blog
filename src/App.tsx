3ffe1274a7c944dafc102f325fc9edc97accce2b
abeer26omar <abeer.omar504@gmail.com>
2024-05-22 18:25:51 +0300

    create: react app - init tailwind - add configrations

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes';
+
+function App() {
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}/>
+    </QueryClientProvider>
+  )
+}
+
+export default App
