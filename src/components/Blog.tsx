c97bf7d4b14b3539ca0a1fb773b557331791204b
abeer26omar <abeer.omar504@gmail.com>
2024-05-23 05:08:41 +0300

    fix: (blog) responsive - scroll animation

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -74,7 +74,7 @@ const Blog = () => {
         <>
             <section className="bg-white pb-6">
                 <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
-                    <div className="container mx-auto px-6 p-6 bg-white">
+                    <div className="container mx-auto px-0 p-0 sm:p-6 bg-white">
                         <div className="mb-16 text-center">
                             <h4 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Posts</h4>
                             <p className="mt-2 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900">The Innovations Never Stop</p>
@@ -100,15 +100,15 @@ const Blog = () => {
                                 return (
                                     <motion.div
                                         initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        transition={{ duration: 0.5, delay: index * 0.1 }} 
-                                        className={`w-full border-b md:w-1/2 p-8 ${applayIndexedStyles()}`} 
+                                        whileInView={{ opacity: 1 }}
+                                        transition={{ duration: 0.5 }} 
+                                        className={`w-full border-b md:w-1/2 sm:p-8 p-3 ${applayIndexedStyles()}`} 
                                         key={post.id}>
                                         <div className="flex items-center mb-6">
                                             <div className="text-xl dynamic_wraper--1">{post.title}</div>
                                         </div>
                                         <p className="leading-loose text-gray-500 dynamic_wraper--3 mb-6">{post.body}</p>
-                                        <div className='flex justify-around'>
+                                        <div className="flex flex-wrap justify-around gap-2">
                                             <button
                                                 type="button"
                                                 onClick={()=>
@@ -129,7 +129,7 @@ const Blog = () => {
                                                         setPostId(post.id)
                                                     }
                                                 }
-                                                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                                className="inline-flex justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:w-auto"
                                             >
                                                 Delete
                                             </button>
